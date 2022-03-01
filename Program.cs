8bf4119e72f42b22dda195b13396ee016f434248
Jessie Hink <jhink1@nait.ca>
2022-03-01 07:52:44 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿/************************************************************************************************************************************/
+//  Purpose:			1. Create a model for the program by drawing a flowchart or writing the pseudo code for the
+//                          program.
+//                      2.Create a test plan and proof of output
+//                      3. Enter the code to implement your model.
+//                      4. Test your code with your test plan. If you discover mistakes, correct the code and model,
+//                          and execute the test plan again
+//
+//  Input()s:			Dimensions of a rectangular room (standard room height is 8 feet)
+//                      Dimensions of a single window, entry door and closet door
+//                      Type of paint
+//                          Basic 29.99 / gallon (covers 300 sq feet on walls and 200 sq feet on ceilings)
+//                          Premium 39.99 / gallon(covers 400 sq feet on walls and 250 sq feet on ceilings)
+//                          Deluxe 49.99 / gallon(covers 500 sq feet on walls and 300 sq feet on ceilings)
+//                      Type of flooring
+//                          Carpet 2.75 / square foot
+//                          Tile 3.50 / square foot
+//                          Hardwood 4.85 / square foot
+//                      Price of Baseboard per linear foot
+//                      Price of Casing per linear foo
+//
+//  Process:			Console.WriteLine()
+//                      Console.ReadLine()
+//
+//  Output(s):			Amount and type of paint required and price
+//                      Amount and type of flooring required and extended price
+//                      Length of Baseboard required and extended price
+//                      Length of Casing required and extended price
+//                      Net Price
+//                      Total GST (5%)
+//                      Total Price
+//
+//  Additional Requirement(s):
+//                      • View supplied video demonstration
+//                      • Calculate a 10 percent waste factor for Baseboard and Casing
+//                      • Paint can only be bought in full gallons
+//                      • Right align the number columns
+//                      • Format prices to two decimal places and all other numbers to one decimal place
+//                      • Input data must be valid positive number or valid choices
+//                      • Continue prompting for a valid value per required input
+//                      • A menu to allow for choice of renovation data to be entered. If the required renovation choice
+//                          already has had data entered, the new data will replace the existing data. Example: you had
+//                          already entered the size of the window, then, decide to change the window size. You may
+//                          choose the window menu choice again and the newly enter data would replace the existing
+//                          window data
+//                      • The packing slip must display zeros for all items that do not have complete input data
+//                      • i.e. no casing price entered... casing footage should also show zero
+//
+//  Test Plan:          Test1________________________________________________Test2________________________________________________Test3________________________________________________
+//                      |       Inputs                                      |        Inputs                                      |        Inputs                                      |
+//                      |                                                   |                                                    |                                                    |
+//                      |       Outputs                                     |        Outputs                                     |        Outputs                                     |
+//                      |___________________________________________________|____________________________________________________|____________________________________________________|   
+//                          
+//                          
+//                          
+//                          
+//
+//  Written by: 		Jessie Hink - jhink1@nait.ca - hijink
+//
+//  Written for: 		CPSC1012E01 - Asad Iqbal
+//
+//  Last modified:		Feb 02, 2021 (PST)
+//
+//
+/************************************************************************************************************************************/
+
+using System;
+//using UserIP;
+
+namespace RenoCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] paintChoices = new string[]{ "A - Basic $29.99 / gallon (covers 300 sq feet on walls and 200 sq feet on ceilings)",
+                                            "B - Premium $39.99 / gallon(covers 400 sq feet on walls and 250 sq feet on ceilings)",
+                                            "D - Deluxe $49.99 / gallon(covers 500 sq feet on walls and 300 sq feet on ceilings)"};
+            string[] flooringChoices = new string[]{"A -  Carpet $2.75 / square foot",
+                                                "B - Tile $3.50 / square foot",
+                                                "C - Hardwood $4.85 / square foot" };
+
+            double[] room = { 0, 0 },
+                        window = new double[]{ 0, 0 },
+                        entryDoor = new double[] { 0, 0 },
+                        closetDoor = new double[] { 0, 0 };
+
+            double castingCost = 0.0,
+                        baseboardCost = 0.0;
+
+            Console.WriteLine(UserIP.GetName("whatever"));
+            //UserIP.GetName("whatever");
+
+
+        }
+
+
+
+    }
+
+}
---
+++ UserIP.cs
@@ -0,0 +1,294 @@
+﻿/********************************************************************************************************/
+//
+/***************************** ADDITIONAL METHODS *******************************************************/
+//
+//
+//      void indent(int spaces)
+//          -indent current line X spaces
+//      string GetName(string defaultName)
+//          -get string from user checks for Null and returns string
+//      int checkChoiceLetter(string[] Choices)
+//          - gets user input and checks for match with first char(letter) of string in all array elements
+//          - requires matching input and returns array index
+//      int checkChoiceNumber(string[] Choices)
+//          - gets user input and checks for match with first char(number) of string in all array elements
+//          - requires matching input and returns array index
+//      bool getYesNo()
+//          -gets input from user and confirms valid
+//          -requires valid input and returns answer in bool
+//      double checkRange(string range)
+//          -gets range of numbers listed in input string
+//          -gets user input, checks validity and returns value within range
+//      double checkRate(string inputString)
+//          - searches input string for dollar amount and outputs amount and double
+//
+/********************************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenoCalculator
+{
+    internal class UserIP
+    {
+
+
+        public static string GetName(string defaultName)
+        {
+            string userIP = Console.ReadLine();
+            if ((userIP != null) && (userIP != ""))
+            {
+                return userIP;
+            }
+            else
+            {
+                return defaultName;
+            }
+        }
+
+        static int checkChoiceLetter(string[] Choices)
+        {
+            bool isValid = false;
+            char userIP;
+            int choice = 0;
+            char[] validIPs = new char[Choices.Length];   //Choices.Length-1
+
+            for (int i = 0; i < Choices.Length; i++)
+            {
+                validIPs[i] = Choices[i].ToCharArray()[0];
+            }
+            do
+            {
+                userIP = Console.ReadKey().KeyChar;
+                if (char.IsLetter(userIP))
+                {
+                    userIP = char.ToUpper(userIP);
+                    for (int i = 0; (i < validIPs.Length) && (!isValid); i++)
+                    {
+                        if (userIP == char.ToUpper(validIPs[i]))
+                        {
+                            choice = i;
+                            isValid = true;
+                        }
+                    }
+                }
+                if (!isValid)
+                {
+                    Console.WriteLine("\nInvalid choice, Please choose from available options");
+                }
+            } while (!isValid);
+
+            return choice;
+        }
+
+        static int checkChoiceNumber(string[] Choices)
+        {
+            bool isValid = false;
+            int choice = 0;
+            int[] validIPs = new int[Choices.Length];
+
+            for (int i = 0; i < Choices.Length; i++)
+            {
+                validIPs[i] = int.Parse(Choices[i].Split('.')[0]);
+            }
+            while (!isValid)
+            {
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    for (int i = 0; (i < validIPs.Length) && !isValid; i++)
+                    {
+                        if (choice == validIPs[i])
+                        {
+                            choice = i;
+                            isValid = true;
+
+                            //return i;
+                        }
+                    }
+                }
+                if (!isValid)
+                {
+                    Console.WriteLine("\nInvalid choice, Please choose from available options");
+                }
+            }
+
+            return choice;
+        }
+
+
+        static bool getYesNo()
+        {
+            bool answer = false,
+                isValid = false;
+            while (!isValid)
+            {
+                string s = Console.ReadLine();
+                if ((s == null) || (s == ""))
+                {
+                    answer = false;
+                    isValid = true;
+                }
+                else if ((bool.TryParse(s, out answer)) && (!isValid)) { isValid = true; }
+                else
+                {
+                    s = s.Trim(' ');
+                    isValid = true;
+                    s = s.ToUpper();
+                    if ((s == "Y") || (s == "YES") || (s == "TRUE")) { answer = true; }
+                    else if ((s == "N") || (s == "NO") || (s == "FALSE")) { answer = false; }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid response.");
+                        isValid = false;
+                    }
+                }
+            }
+            return answer;
+        }
+
+        static double checkRange(string range)
+        {
+            char[] delimiterChars = { '[', '-', ']' };
+            bool isValid = false;
+            double lowerLimit,
+                    upperLimit,
+                    userNumber = 0.0;
+
+            string[] stringSegment = range.Split(delimiterChars);
+            lowerLimit = double.Parse(stringSegment[1]);
+            upperLimit = double.Parse(stringSegment[2]);
+
+            do
+            {
+                if (Double.TryParse(Console.ReadLine(), out double n))
+                {
+                    isValid = true;
+                    if (n > upperLimit)
+                    {
+                        userNumber = upperLimit;
+                    }
+                    else if (n < lowerLimit)
+                    {
+                        userNumber = lowerLimit;
+                    }
+                    else
+                    {
+                        userNumber = n;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please input a number.");
+                }
+            } while (!isValid);
+
+
+            return (userNumber);
+        }
+
+        static double checkRate(string inputString)
+        {
+            char[] delimiterChars = { ' ', ']' };
+            string s = inputString.Split('$')[1];
+            return (double.Parse(s.Split(delimiterChars)[0]));
+        }
+
+
+        static int getInt(int Lowest, int Highest)
+        {
+            bool validData = false;
+            int userIP = 0;
+            while (!validData)
+            {
+                try
+                {
+                    userIP = int.Parse(Console.ReadLine());
+                    if ((userIP >= Lowest) && (userIP <= Highest))
+                        validData = true;
+                    else
+                        Console.WriteLine($"Error: Entry Out of Range\n  -number must be {Lowest} or higher and {Highest} or Lower.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: Not Valid Data:\n{e.Message}");
+                }
+            }
+            return userIP;
+        }
+
+        static int getInt(int Lowest)
+
+        {
+            bool validData = false;
+            int userIP = 0;
+            while (!validData)
+            {
+                try
+                {
+                    userIP = int.Parse(Console.ReadLine());
+                    if (userIP >= Lowest)
+                        validData = true;
+                    else
+                        Console.WriteLine($"Error: Entry Out of Range\n  -number must be {Lowest} or higher.");
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: Not Valid Data:\n{e.Message}");
+                }
+            }
+            return userIP;
+        }
+
+        static double getDouble(double Lowest, double Highest)
+
+        {
+            bool validData = false;
+            double userIP = 0.0;
+            while (!validData)
+            {
+                try
+                {
+                    userIP = double.Parse(Console.ReadLine());
+                    if ((userIP >= Lowest) && (userIP <= Highest))
+                        validData = true;
+                    else
+                        Console.WriteLine($"Error: Entry Out of Range\n  -number must be {Lowest} or higher and {Highest} or Lower.");
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: Not Valid Data:\n{e.Message}");
+                }
+            }
+            return userIP;
+        }
+
+        static double getDouble(double Lowest)
+
+        {
+            bool validData = false;
+            double userIP = 0.0;
+            while (!validData)
+            {
+                try
+                {
+                    userIP = double.Parse(Console.ReadLine());
+                    if (userIP >= Lowest)
+                        validData = true;
+                    else
+                        Console.WriteLine($"Error: Entry Out of Range\n  -number must be {Lowest} or higher.");
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: Not Valid Data:\n{e.Message}");
+                }
+            }
+            return userIP;
+        }
+    }
+}
+
